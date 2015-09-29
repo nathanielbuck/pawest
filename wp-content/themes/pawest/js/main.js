10dfa395582de45c6de2dc4edc65c1fda2ffee78
Nathaniel Buck <nathaniel.buck@gmail.com>
2015-09-28 23:25:28 -0400

    Add Styling and JavaScript for Q&As

--- wp-content/themes/pawest/js/main.js
+++ wp-content/themes/pawest/js/main.js
@@ -18,4 +18,22 @@ $(document).ready(function () {
 		}
 		return false;
 	});
+	$('.qa_radio').click(function () {
+		var $this = $(this);
+		var $qa = $this.closest('.qa');
+		var $result = $qa.find('.qa_result');
+		var result_message = '';
+
+		if ($this.parent('.qa_answer').hasClass('qa_answer--correct')) {
+			result_message += 'Correct!';
+			$qa.addClass('qa--correct');
+			$qa.find('.qa_label, .qa_radio').prop('disabled', true);
+		}
+
+		else {
+			result_message += 'Incorrect. Try again.';
+		}
+
+		$result.text(result_message);
+	});
 });
